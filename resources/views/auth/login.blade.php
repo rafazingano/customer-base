f47f816845d45f6e4437f56b9fdd0c3124d91249
Rafael Zingano <rafazingano@gmail.com>
2017-10-31 01:52:12 -0200

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -83,16 +83,16 @@
   <div class="container-fluid">
       <div class="row" style=" width: 100%;">
             <div class="col-md-6 col-12" style="padding: 4px 5px;">
-                <img src="assets/images/logo-2.png" class="img-fluid" style="    margin-top: 4px;float:left; width: 180px;">
-                <p style="margin-left: 10px;float:left; margin-bottom: 4px; padding-top: 0px;">Copyright  2017 - Algo mais gráfica e editora
+                <img src="assets/images/logo-2.png" class="img-fluid" style=" margin-top: 4px;float:left; max-width: 42%;">
+                <p style="max-width: 54%;margin-left: 10px;float:left; margin-bottom: 4px; padding-top: 0px;">Copyright  2017 - Algo mais gráfica e editora
                     <br>
                 Todos os direitos reservados</p>
             </div>
-            <div class="col-md-2 col-6" style="padding: 4px 5px;">
+            <div class="col-md-2 col-12" style="padding: 4px 5px;">
                 <img src="assets/images/phone.png" class="img-fluid" style="    margin: 4px;height: 40px;float: left;">
                 <p style="margin-bottom: 4px; padding-top: 0px;"><span style="font-size: 14px;">Fone (51)</span> <b>3222 2339</b></p>
             </div>
-            <div class="col-md-4 col-6" style="padding: 4px 5px;">
+            <div class="col-md-4 col-12" style="padding: 4px 5px;">
                 <img src="assets/images/map.png" class="img-fluid" style="    margin: 4px;height: 40px;float: left;">
                 <p style="margin-bottom: 4px; padding-top: 0px;">Rua  Santos Dumount, nº 1101 São Geraldo</p>
                 <p style="margin-bottom: 4px;">Cep 90230-241 - Porto Alegre,RS</p>
